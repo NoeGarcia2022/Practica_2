882b3a025c9fcd6909b48f470a2257c5e8a11ea7
salvaVE <valladares.salvador12@gmail.com>
2022-05-05 09:23:45 -0600

    Case read

--- CRUD_Estudiante/src/crud_estudiante/Read.java
+++ CRUD_Estudiante/src/crud_estudiante/Read.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package crud_estudiante;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.SQLException;
+
+
 public class Read {
+    public Read() throws SQLException{
+        System.out.println("<<CONSULTA DE REGISTROS>>");
+        mostrarResultados();//Llamar el metodo dentro de esta clase 
     
 }
+    }
+
+private void mostrarResultados() throws SQLException{
+        try{
+            ConexionCRUD utilerias=new ConexionCRUD();
+            String tabla="tb_datos_estudiantes";
+            String camposTabla="*";
+            //Conexion se envia vacia para indicar que todos los registros se necesitan mostrar
+
+            String condicionBusqueda="";
+            //Metodo que realiza la busqueda
+            utilerias.desplegarRegistro(tabla, camposTabla, condicionBusqueda);
