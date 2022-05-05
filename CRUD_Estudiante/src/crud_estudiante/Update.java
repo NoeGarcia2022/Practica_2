8d58f8a557c9147b38a4f12d6257536412920ad9
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-05-05 09:55:15 -0600

    Clase Update

--- CRUD_Estudiante/src/crud_estudiante/Update.java
+++ CRUD_Estudiante/src/crud_estudiante/Update.java
@@ -1,7 +1,15 @@
 
 package crud_estudiante;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 
 public class Update {
+    Update() throws SQLException {
+        Scanner leer = new Scanner(System.in);
+        Estudiante est = new Estudiante();
+        ConexionCRUD utilerias = new ConexionCRUD();
+        System.out.println("<<Actualizar Registros>>");
     
 }
